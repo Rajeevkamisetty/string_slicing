4a4dfcf93898f3b076998a0bf0a44fb1cdcbb73d
Rajeevkamisetty <145850864+Rajeevkamisetty@users.noreply.github.com>
2023-10-13 16:27:32 +0530

    Add files via upload

---
+++ 13-10-2023.py
@@ -0,0 +1,18 @@
+# string slicing
+
+name = "rajeev"
+print(name[0:4])
+print(name[-5:-1])
+
+# reverse a string
+name = "rajeev"
+print(name[::-1])
+
+# reverse a list
+names = ["rajeev", "sai", "pretham", "basha"]
+print(names[::-1])
+
+# reverse a list without slicing
+names = ["rajeev", "sai", "pretham", "basha"]
+names.reverse()
+print(names)
